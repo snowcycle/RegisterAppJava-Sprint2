d8105ca66a63ee0364bf499c68c2dfb62cd78aee
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-07 20:18:05 -0500

    Added a few lines to transaction controller

--- src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -1,6 +1,29 @@
 package edu.uark.registerapp.controllers;
 
-public class TransactionRouteController extends BaseRouteController
-{
+import java.util.Map;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+
+@Controller
+@RequestMapping(value = "/transaction")
+public class TransactionRouteController extends BaseRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+  }
 }
+
+	
